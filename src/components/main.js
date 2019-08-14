c73a2d1d9003b6e1ad7fc4d830d620f010d9a381
r0y09 <roy.indrajeet009@gmail.com>
2019-08-14 17:22:34 +0400

    thrid

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import LandingPage from "./landingpage";
 import AboutMe from "./aboutme";
