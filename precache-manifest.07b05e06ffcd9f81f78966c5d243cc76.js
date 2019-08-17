533a6d528e8758b86f4b417d5f868894ba814149
r0y09 <roy.indrajeet009@gmail.com>
2019-08-18 02:24:34 +0400

    Updates

---
+++ precache-manifest.07b05e06ffcd9f81f78966c5d243cc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11017d7753deae09708f7ba97cc25ca",
+    "url": "/Myportfolio/index.html"
+  },
+  {
+    "revision": "65858ede9ec7d3114e30",
+    "url": "/Myportfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "fbf92806826ad8607c1d",
+    "url": "/Myportfolio/static/css/main.1c2e4cce.chunk.css"
+  },
+  {
+    "revision": "65858ede9ec7d3114e30",
+    "url": "/Myportfolio/static/js/2.fedd8479.chunk.js"
+  },
+  {
+    "revision": "fbf92806826ad8607c1d",
+    "url": "/Myportfolio/static/js/main.4d378029.chunk.js"
+  },
+  {
+    "revision": "d216945e8ad704eb346d",
+    "url": "/Myportfolio/static/js/runtime~main.8ffb7fd8.js"
+  }
+]);
