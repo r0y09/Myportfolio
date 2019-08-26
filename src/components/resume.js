47a25d30eff6a0c4f1a285fb1b8e7ce81a24bb8f
r0y09 <roy.indrajeet009@gmail.com>
2019-08-25 19:17:46 -0500

    1

--- src/components/resume.js
+++ src/components/resume.js
@@ -71,7 +71,7 @@ class Resume extends Component {
             pointBorderColor: "#fff",
             pointHoverRadius: 10,
             pointHoverColor: "red",
-            data: [3, 3, 3, 4, 1, 2, 3, 4, 4, 1, 2, 2]
+            data: [3, 3, 3, 4, 2, 2, 3, 4, 4, 1, 2, 2]
           }
         ]
       },
